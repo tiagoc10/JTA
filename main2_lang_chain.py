19cf033560078889813a3e8000ebbd81edb3efb9
tiagoc10 <tiagoamcouto@gmail.com>
2025-07-20 15:31:48 +0100

    task2: Cleaning code

--- main2_lang_chain.py
+++ main2_lang_chain.py
@@ -1,48 +1,81 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 import os
-from vector import retriever
+from vector import create_llm
 
 from config import OPENAI_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-model = ChatOpenAI(model_name="gpt-3.5-turbo",
-                   temperature=0.3,        # Menor temperature → respostas mais focadas
-                   )
 
-template = """
-You are an expert on videogames, their sales history, and gaming consoles.
-You will be provided with a list of videogames along with their sales history and reviews.
-Answer the user's question strictly based on this information.
-Your language should be clear and concise, avoiding unnecessary details.
-You need to answer in same language as the question, so if the question is in Portuguese, you should answer in Portuguese.
-If the question is in English, you should answer in English.
+def chatbot(xlsx_file, json_file):
+    """
+    Start a chatbot that uses the LLM to answer questions based on the\
+        provided xlsx and json files.
+    Args:
+        xlsx_file (str): Path to the xlsx file containing co-occurrence data.
+        json_file (str): Path to the json file containing product data.
+    Raises:
+        ValueError: If the xlsx or json file paths are not provided.
+    Returns:
+        None
+    This function initializes a chatbot that listens for user input and\
+        responds based on the provided data.
+    It uses the LLM to process the input and generate responses based on the\
+        context of the provided xlsx and json files.
+    The chatbot will continue to run until the user decides to exit by typing\
+        'q', 'quit', or 'exit'.
+    """
+    retriever = create_llm(xlsx_file, json_file)
+    model = ChatOpenAI(model_name="gpt-3.5-turbo",
+                       temperature=0.3,
+                       )  # Lower temperature value → more focused responses
 
-Guidelines:
-- If the question is unrelated to videogame sales, consoles, or reviews, respond politely that you don't know.
-- If the question is ambiguous or unclear, ask for clarification politely.
-- If the user asks for opinions or subjective judgments, say you cannot proivde opinions because you are an AI.
-- If the question asks about information not in the data, respond politely that you don't have that information.
-- For greetings, respond politely.
-- For farewells, respond politely.
-- For questions outside your expertise, respond politely that you dont have the related information.
+    template = """
+    You are an expert on videogames, their sales history, and gaming consoles.
+    You will be provided with a list of videogames along with their sales\
+        history and reviews.
+    Answer the user's question strictly based on this information.
+    Your language should be clear and concise, avoiding unnecessary details.
+    You need to answer in same language as the question, so if the question is\
+        in Portuguese, you should answer in Portuguese.
+    If the question is in English, you should answer in English.
 
-Here are some relevant reviews: {reviews}
+    Guidelines:
+    - If the question is unrelated to videogame sales, consoles, or reviews,\
+        respond politely that you don't know.
+    - If the question is ambiguous or unclear, ask for clarification politely.
+    - If the user asks for opinions or subjective judgments, say you cannot\
+        provide opinions because you are an AI.
+    - If the question asks about information not in the data, respond politely\
+        that you don't have that information.
+    - For greetings, respond politely.
+    - For farewells, respond politely.
+    - For questions outside your expertise, respond politely that you dont\
+        have the related information.
 
-Here is the question to answer: {question}
-"""
+    Here are some relevant reviews: {reviews}
 
-prompt = ChatPromptTemplate.from_template(template)
-chain = prompt | model
+    Here is the question to answer: {question}
+    """
 
-print("Chatbot online! Press 'q' or 'quit' or 'exit' to exit")
-while True:
-    print("\n---------------------------\n")
-    question = input("User: ")
-    print("\n")
-    if question.lower() in ["q", "quit", "exit"]:
-        print("Exiting the chatbot. Goodbye!")
-        break
-    reviews = retriever.invoke(question)  # Retrieve relevant documents based on the question
-    result = chain.invoke({"reviews": reviews, "question": question})
-    print(f"Bot: {result.content}")  # Print the answer from the model
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | model
+
+    print("Chatbot online! Press 'q' or 'quit' or 'exit' to exit")
+    while True:
+        print("\n---------------------------\n")
+        question = input("User: ")
+        print("\n")
+        if question.lower() in ["q", "quit", "exit"]:
+            print("Exiting the chatbot. Goodbye!")
+            break
+        # Retrieve relevant documents based on the question
+        reviews = retriever.invoke(question)
+        result = chain.invoke({"reviews": reviews, "question": question})
+        print(f"Bot: {result.content}")  # Print the answer from the model
+
+
+if __name__ == "__main__":
+    xlsx_file = "Nintendo_Cooccurrence_Matrix.xlsx"
+    json_file = "dataset.json"
+    chatbot(xlsx_file, json_file)
--- vector.py
+++ vector.py
@@ -8,111 +8,136 @@ from config import OPENAI_API_KEY
 
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-db_location = "./chroma_db_langchain"
-add_documents = not os.path.exists(db_location)
 
-df = pd.read_excel("Nintendo_Cooccurrence_Matrix.xlsx", index_col=0)
+def create_llm(xlsx_file, json_file):
+    """
+    Create a retriever for the LLM using the provided xlsx and json files.
+    Args:
+        xlsx_file (str): Path to the xlsx file containing co-occurrence data.
+        json_file (str): Path to the json file containing product data.
+    Returns:
+        retriever: A retriever object that can be used to query the LLM.
+    Raises:
+        ValueError: If the xlsx or json file paths are not provided.
+    """
+    if not xlsx_file:
+        raise ValueError("XLSX file path must be provided.")
+    if not json_file:
+        raise ValueError("JSON file path must be provided.")
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    db_location = "./chroma_db_langchain"
+    add_documents = not os.path.exists(db_location)
 
-documents = []
-ids = []
+    df = pd.read_excel("Nintendo_Cooccurrence_Matrix.xlsx", index_col=0)
 
-# Xlsx data processing
-if add_documents:
-    for i, row_product in enumerate(df.index):
-        for j, col_product in enumerate(df.columns):
-            if j < i:
-                continue
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
-            value = df.iloc[i, j]
-            if row_product == col_product:
-                document_text = f"{row_product} was sold alone {value} times"
-            else:
-                products = sorted([row_product, col_product])
-                document_text = f"{products[0]} was sold together with {products[1]} {value} times"
+    documents = []
+    ids = []
 
-            doc_id = f"{sorted([row_product, col_product])[0]}__{sorted([row_product, col_product])[1]}"
-            document = Document(
-                page_content=document_text,
-                metadata={
-                    "product_a": row_product,
-                    "product_b": col_product,
-                    "Number of sales": int(value)
-                },
-                id=doc_id
-            )
-            documents.append(document)
-            ids.append(doc_id)
+    # Xlsx data processing
+    if add_documents:
+        for i, row_product in enumerate(df.index):
+            for j, col_product in enumerate(df.columns):
+                if j < i:
+                    continue
 
-# JSON data processing
-if add_documents:
-    with open("dataset.json", "r", encoding="utf-8") as f:
-        product_data = json.load(f)
+                value = df.iloc[i, j]
+                if row_product == col_product:
+                    document_text = f"{row_product} was sold alone {value} times"
+                else:
+                    products = sorted([row_product, col_product])
+                    document_text = f"{products[0]} was sold together with {products[1]} {value} times"
 
-    for category, products in product_data.items():
-        for product in products:
-            name = product.get("name")
-            times_sold = product.get("times_sold", 0)
+                doc_id = f"{sorted([row_product, col_product])[0]}__{sorted([row_product, col_product])[1]}"
+                document = Document(
+                    page_content=document_text,
+                    metadata={
+                        "product_a": row_product,
+                        "product_b": col_product,
+                        "Number of sales": int(value)
+                    },
+                    id=doc_id
+                )
+                documents.append(document)
+                ids.append(doc_id)
 
-            description_parts = []
+    # JSON data processing
+    if add_documents:
+        with open("dataset.json", "r", encoding="utf-8") as f:
+            product_data = json.load(f)
 
-            if category == "Console":
-                description_parts.append(f"Console {name} was sold {times_sold} times.")
-                for store in ["Store A", "Store B", "Store C"]:
-                    if store in product and product[store] is not None:
-                        description_parts.append(f"Console {name} was sold in {store} {product[store]} times.")
-                    elif store in product and product[store] is None:
-                        description_parts.append(f"{store} did not sell {name} console.")
+        for category, products in product_data.items():
+            for product in products:
+                name = product.get("name")
+                times_sold = product.get("times_sold", 0)
 
-            elif category == "Games":
-                description_parts.append(f"Game {name} was sold {times_sold} times.")
-                for store in ["Store A", "Store B", "Store C"]:
-                    if store in product and product[store] is not None:
-                        description_parts.append(f"Game {name} was sold in {store} {product[store]} times.")
-                    elif store in product and product[store] is None:
-                        description_parts.append(f"{store} did not sell {name} game.")
-                if "release_date" in product:
-                    description_parts.append(f"Game {name} was released in {product['release_date']}.")
-                if "type" in product:
-                    description_parts.append(f"Game {name} is {product['type']} type.")
-                if "franchise" in product:
-                    description_parts.append(f"{name} game belongs to franchise {product['franchise']}.")
-                if "min_age" in product:
-                    description_parts.append(f"The minimum age required to play {name} is {product['min_age']} years old.")
+                description_parts = []
 
-            elif category == "Accessories":
-                description_parts.append(f"Accessory {name} was sold {times_sold} times.")
-                for store in ["Store A", "Store B", "Store C"]:
-                    if store in product and product[store] is not None:
-                        description_parts.append(f"Accessory {name} was sold in {store} {product[store]} times.")
-                    elif store in product and product[store] is None:
-                        description_parts.append(f"{store} did not sell {name} acessory.")
-                if "category" in product:
-                    description_parts.append(f"Accessory {name} is a {product['category']}.")
+                if category == "Console":
+                    description_parts.append(f"Console {name} was sold {times_sold} times.")
+                    for store in ["Store A", "Store B", "Store C"]:
+                        if store in product and product[store] is not None:
+                            description_parts.append(f"Console {name} was sold in {store} {product[store]} times.")
+                        elif store in product and product[store] is None:
+                            description_parts.append(f"{store} did not sell {name} console.")
 
-            description = " ".join(description_parts)
+                elif category == "Games":
+                    description_parts.append(f"Game {name} was sold {times_sold} times.")
+                    for store in ["Store A", "Store B", "Store C"]:
+                        if store in product and product[store] is not None:
+                            description_parts.append(f"Game {name} was sold in {store} {product[store]} times.")
+                        elif store in product and product[store] is None:
+                            description_parts.append(f"{store} did not sell {name} game.")
+                    if "release_date" in product:
+                        description_parts.append(f"Game {name} was released in {product['release_date']}.")
+                    if "type" in product:
+                        description_parts.append(f"Game {name} is {product['type']} type.")
+                    if "franchise" in product:
+                        description_parts.append(f"{name} game belongs to franchise {product['franchise']}.")
+                    if "min_age" in product:
+                        description_parts.append(f"The minimum age required to play {name} is {product['min_age']} years old.")
 
-            doc_id = f"{category}_{name.replace(' ', '_')}"
-            document = Document(
-                page_content=description,
-                metadata={
-                    "name": name,
-                    "category": category,
-                    "times_sold": times_sold
-                },
-                id=doc_id
-            )
-            documents.append(document)
-            ids.append(doc_id)
+                elif category == "Accessories":
+                    description_parts.append(f"Accessory {name} was sold {times_sold} times.")
+                    for store in ["Store A", "Store B", "Store C"]:
+                        if store in product and product[store] is not None:
+                            description_parts.append(f"Accessory {name} was sold in {store} {product[store]} times.")
+                        elif store in product and product[store] is None:
+                            description_parts.append(f"{store} did not sell {name} acessory.")
+                    if "category" in product:
+                        description_parts.append(f"Accessory {name} is a {product['category']}.")
 
-vector_store = Chroma(
-    collection_name="My_data",
-    persist_directory=db_location,
-    embedding_function=embeddings,
-)
+                description = " ".join(description_parts)
 
-if add_documents:
-    vector_store.add_documents(documents, ids=ids)
+                doc_id = f"{category}_{name.replace(' ', '_')}"
+                document = Document(
+                    page_content=description,
+                    metadata={
+                        "name": name,
+                        "category": category,
+                        "times_sold": times_sold
+                    },
+                    id=doc_id
+                )
+                documents.append(document)
+                ids.append(doc_id)
 
-retriever = vector_store.as_retriever(search_kwargs={"k": 5})
+    vector_store = Chroma(
+        collection_name="My_data",
+        persist_directory=db_location,
+        embedding_function=embeddings,
+    )
+
+    if add_documents:
+        vector_store.add_documents(documents, ids=ids)
+
+    retriever = vector_store.as_retriever(search_kwargs={"k": 5})
+
+    return retriever
+
+
+if __name__ == "__main__":
+    xlsx_file = "Nintendo_Cooccurrence_Matrix.xlsx"
+    json_file = "dataset.json"
+    retriever = create_llm(xlsx_file, json_file)
